c76aba0896e22d69d56c673e39f004085df140aa
okezino <okezi002@gmail.com>
2022-07-05 23:58:00 +0100

    merge-sort

--- src/SortAlgo.kt
+++ src/SortAlgo.kt
@@ -37,19 +37,111 @@ object SortAlgo {
         return arr
     }
 
-    fun countingSort(arr : Array<Int>){
+
+    fun quickSort(arr : Array<Int>) {
+        if(arr.size < 2) return
+        var mid =  arr.size / 2
+        val left = Array(mid){0}
+        val right = Array(arr.size - mid){0}
+        for (i in 0 until mid) left[i] = arr[i]
+        for (i in mid  until arr.size) right[i - mid] = arr[i]
+
+
+        quickSort(left)
+        quickSort(right)
+        merges(arr, left, right)
+    }
+
+    private fun merges(arr: Array<Int>, left: Array<Int>, right: Array<Int>){
+        val leftSide = left.size
+        val rightSide = right.size
+        var leftIndex = 0
+        var rightIndex = 0
+        var index = 0
+
+
+        while( leftIndex < leftSide && rightIndex < rightSide ){
+            if(left[leftIndex] <= right[rightIndex]){
+                arr[index] = left[leftIndex]
+                leftIndex++
+            }else{
+                arr[index] = right[rightIndex]
+                rightIndex++
+            }
+            index++
+        }
+
+        while(leftIndex < leftSide){
+            arr[index] = left[leftIndex]
+            leftIndex++
+            index++
+        }
+
+        while(rightIndex < rightSide){
+            arr[index] = right[rightIndex]
+            rightIndex++
+            index++
+        }
+        println(left.toList())
+        println(right.toList())
+        println(arr.toList())
+        println("=======================")
+
 
     }
 
-    fun quickSort(arr : Array<Int>){
+    fun mergeSort(arr : Array<Int>) : Array<Int>{
+        var temp = arr
+        mergeSorted(arr, temp,0, arr.size-1)
+
+        return temp
+    }
+
+    private fun mergeSorted(arr : Array<Int>, temp : Array<Int>, leftStart : Int, rightEnd:Int){
+        if(leftStart >= rightEnd) return
+        val mid = (leftStart + rightEnd) / 2
+        mergeSorted(arr,temp, leftStart, mid)
+        mergeSorted(arr, temp,mid + 1, rightEnd)
+        mergeHalves(arr, temp, leftStart, rightEnd)
 
     }
 
-    fun mergeSort(arr : Array<Int>){
+    private fun mergeHalves(arr : Array<Int>, array: Array<Int>, leftStart : Int, rightEnd :Int){
+        var leftEnd = (leftStart + rightEnd) / 2
+        var rightStart = leftEnd + 1
+        var size = rightEnd - leftStart + 1
+
+        var left = leftStart
+        var right = rightStart
+        var index = leftStart
+
+        while (
+            left <= leftEnd && right <= rightEnd
+        ){
+            if(arr[left] <= arr[right]){
+                array[index] = arr[left]
+                left++
+            }else{
+                array[index] = arr[right]
+                right++
+            }
+
+            index++
+        }
+
+
+        while ( left <= leftEnd ){
+            array[index] = arr[left]
+            left++
+            index++
+        }
+        while (right <= rightEnd){
+            array[index] = arr[right]
+            right++
+            index++
+        }
+
 
     }
 
-    fun heapSort(arr : Array<Int>){
-
-    }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,8 @@
 fun main() {
 
     println(SortAlgo.bubbleSort(arrayOf(5,2,2,10,4,8)).toList())
-    println(SortAlgo.insertionSort(arrayOf()).toList())
+    println(SortAlgo.insertionSort(arrayOf(5,2,2,10,4,8)).toList())
+   SortAlgo.quickSort(arrayOf(5,2,2,10,4,8))
 }
 
 
